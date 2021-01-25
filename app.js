30a86d9bdd3860a62076e38684c755e86b40096e
Kapil <choudharykapil776@gmail.com>
2021-01-25 21:52:47 +0530

    changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require( 'body-parser' );
 const morgan = require( 'morgan' );
 const usersRoute = require('./src/routers/user');
 const postRoute = require('./src/routers/post')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const uri = "mongodb+srv://admin:1234567890@cluster0.s11is.mongodb.net/socialmedia?retryWrites=true&w=majority";
 
 
@@ -16,20 +16,13 @@ app.use( morgan( 'dev' ) );
 app.use(cors())
 
 const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+
 
 
 //server Run on port 3000
 app.listen( port, ( err ) => {
   if ( err ) { console.error( err ); }
-  console.log( 'Listening on 3000' );
+  console.log( 'Listening on 5000' );
 } );
 
 // Use connect method to connect to the server
@@ -58,6 +51,13 @@ app.use( '/api/user', usersRoute );
 
 app.use( '/api/post', postRoute );
 
-  
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
   module.exports = app;
   
